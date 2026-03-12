4e891407848e4a64922511c39646500fb7782ab2
tatoloren <tomas.lorenzo.2007@gmail.com>
2026-03-11 21:33:08 -0300

    Mi primer commit

---
+++ repaso.py
@@ -0,0 +1,17 @@
+# Comentarios en linea
+""" 
+Comentarios en bloque
+"""
+print("Calculadora en Python")
+
+# Inizializar variables
+# numero1 = 4
+# numero2 = 6
+numero1 = 4
+numero2 = 6
+
+# Sumar los numeros
+suma = numero1 + numero2
+
+# Mostrar el resultado
+print(f"El resultado de la suma es: {suma}") 
